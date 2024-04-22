de8cdf68e7edcd8bd81bd1469b20d8bf7f069207
RaphaelIT7 <raphael.i.t@outlook.de>
2024-04-23 00:46:30 +0200

    Removed wrong changes

--- public/GarrysMod/Gamemode.h
+++ public/GarrysMod/Gamemode.h
@@ -28,18 +28,14 @@ class System
 {
 public:
 	virtual void OnJoinServer( const std::string & ) = 0;
-#ifndef GMOD_DEDICATED
 	virtual void OnLeaveServer( ) = 0;
-#endif
 	virtual void Refresh( ) = 0;
 	virtual void Clear( ) = 0;
 	virtual const IGamemodeSystem::Information &Active( ) = 0;
 	virtual const IGamemodeSystem::Information &FindByName( const std::string & ) = 0; // Guessing that it returns IGamemodeSystem::Information
 	virtual void SetActive( const std::string & ) = 0;
 	virtual const std::list<IGamemodeSystem::Information> &GetList( ) const = 0;
-#ifndef GMOD_DEDICATED
 	virtual bool IsServerBlacklisted( char const* address, char const* hostname, char const* description, char const* gm, char const* map ) = 0;
-#endif
 };
 
 }
