d61d974d9a0ddd482aaca7b541971a522c4412be
RaphaelIT7 <raphael.i.t@outlook.de>
2024-06-06 04:02:44 +0200

    Fixed some compile errors + Raised MAX_PLAYERS to 128

--- engine/host.cpp
+++ engine/host.cpp
@@ -1293,7 +1293,11 @@ void Host_WriteConfiguration( const char *filename, bool bAllVars )
 
 		if ( pRemoteStorage )
 		{
+#ifdef BUILD_GMOD
+			uint64 availableBytes, totalBytes = 0;
+#else
 			int32 availableBytes, totalBytes = 0;
+#endif
 			if ( pRemoteStorage->GetQuota( &totalBytes, &availableBytes ) )
 			{
 				if ( totalBytes > 0 )
