660494964323b3a6a26e9a35c47b8249d4898447
RaphaelIT7 <raphael.i.t@outlook.de>
2024-06-07 23:35:19 +0200

    Moving stuff around to their proper place

--- GarrysMod/Lua/custom_libs/gl_message.cpp
+++ GarrysMod/Lua/custom_libs/gl_message.cpp
@@ -1,1 +1,19 @@
 #include "cbase.h"
+#include "gl_message.h"
+#include "usermessages.h"
+
+void RecieveLuaUserMessage( bf_read& msg )
+{
+	// ToDo
+
+	// call usermessage.IncomingMessage
+}
+
+void RegisterLuaUserMessages()
+{
+	usermessages->Register( "LuaUserMessage", -1 );
+
+#ifdef CLIENT_DLL
+	usermessages->HookMessage( "LuaUserMessage", RecieveLuaUserMessage );
+#endif
+}
