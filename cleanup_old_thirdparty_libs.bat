b0e3562525770325a4f9f6d4e0e7bb8be38b3071
march <106459595+marchc1@users.noreply.github.com>
2024-11-24 18:30:24 -0800

    Add /thirdparty/ lib output cleanup
    if someone else needs it

---
+++ cleanup_old_thirdparty_libs.bat
@@ -0,0 +1,6 @@
+RMDIR /S thirdparty\libjpeg-turbo\out
+RMDIR /S thirdparty\libpng\out
+RMDIR /S thirdparty\mpaheaderinfo\out
+RMDIR /S thirdparty\SDL\out
+RMDIR /S thirdparty\snappy\out
+RMDIR /S thirdparty\zlib\out
